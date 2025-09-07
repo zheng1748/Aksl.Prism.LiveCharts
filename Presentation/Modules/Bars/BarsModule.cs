fec24a246b49fdd89de5ed6e3b5e424cdede1303
zheng1748 <zheng1748@outlook.com>
2025-09-07 20:48:17 +0800

    Bars

---
+++ Presentation/Modules/Bars/BarsModule.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using Unity;
+
+using Aksl.Modules.LiveCharts.Bars.ViewModels;
+using Aksl.Modules.LiveCharts.Bars.Views;
+using System;
+
+//install-package LiveChartsCore.SkiaSharpView.WPF -Version 2.0.0-beta.90
+//install-package LiveChartsCore.SkiaSharpView.WPF--version 2.0.0-rc5.4
+
+namespace Aksl.Modules.LiveCharts.Bars
+{
+    public class BarsModule : IModule
+    {
+        #region Members
+        private readonly IUnityContainer _container;
+        #endregion
+
+        #region Constructors
+        public BarsModule(IUnityContainer container)
+        {
+            this._container = container;
+        }
+        #endregion
+
+        #region IModule 成员
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<AutoUpdateView>();
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            ViewModelLocationProvider.Register(typeof(AutoUpdateView).ToString(),
+                                         () => this._container.Resolve<AutoUpdateViewModel>());
+        }
+        #endregion
+    }
+}
