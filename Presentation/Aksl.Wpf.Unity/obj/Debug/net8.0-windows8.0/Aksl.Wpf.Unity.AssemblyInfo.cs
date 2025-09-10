189d747659e089c99e6b5cf0f5863c2ac34a4eb8
zheng1748 <zheng1748@outlook.com>
2025-09-10 19:38:07 +0800

    Axes

--- Presentation/Aksl.Wpf.Unity/obj/Debug/net8.0-windows8.0/Aksl.Wpf.Unity.AssemblyInfo.cs
+++ Presentation/Aksl.Wpf.Unity/obj/Debug/net8.0-windows8.0/Aksl.Wpf.Unity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aksl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7411262d951787bc631f76777ee27c36cc5b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aksl.Wpf.Unity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aksl.Wpf.Unity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
