15341a46eb7ef07c1d07a9f83ee5f1bd53746a12
zheng1748 <zheng1748@outlook.com>
2025-09-11 10:21:21 +0800

    HamburgerMenu

--- Presentation/Modules/Navigations/HamburgerMenuView/HamburgerMenuModule.cs
+++ Presentation/Modules/Navigations/HamburgerMenuView/HamburgerMenuModule.cs
@@ -30,8 +30,6 @@ namespace Aksl.Modules.HamburgerMenu
             containerRegistry.RegisterForNavigation<IndustryHamburgerMenuHubView>();
 
             containerRegistry.RegisterForNavigation<CustomerHamburgerMenuHubView>();
-
-            //containerRegistry.RegisterForNavigation<AxesHamburgerMenuHubView>();
         }
 
         public void OnInitialized(IContainerProvider containerProvider)
@@ -44,9 +42,6 @@ namespace Aksl.Modules.HamburgerMenu
 
             ViewModelLocationProvider.Register(typeof(CustomerHamburgerMenuHubView).ToString(),
                                            () => this._container.Resolve<CustomerHamburgerMenuHubViewModel>());
-
-            //ViewModelLocationProvider.Register(typeof(AxesHamburgerMenuHubView).ToString(),
-            //                              () => this._container.Resolve<AxesHamburgerMenuHubViewModel>());
         }
         #endregion
     }
